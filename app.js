23d4c5ae62017c69e8d37b21c8845512b4b2bfb1
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-01-26 00:16:06 +0100

    Challenge 1 completed

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"))
 
+app.get("/", (req, res) => {
+	res.render("home")
+})
+
 app.listen(4000, () => {
 	console.log("App is listening on port 4000")
 })
