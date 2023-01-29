5c345cff4d96837412625b65ed417f8b86ce04e8
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-01-29 20:46:19 +0100

    Challenge 20 completed. Substring used to truncate blogs on Homepage

--- app.js
+++ app.js
@@ -24,6 +24,13 @@ app.get("/", (req, res) => {
 	// res.render("home", { homeStartingContentEjs: homeStartingContent })
 	res.render("home", { homeStartingContentEjs: homeStartingContent, posts: posts })
 	// console.log(posts)
+
+	function truncateString(str, num) {
+		if (str.length <= num) {
+			return str
+		}
+		return str.slice(0, num) + "..."
+	}
 })
 
 //about route
